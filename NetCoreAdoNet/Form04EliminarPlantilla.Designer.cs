8569512bc0c8e897b2bfd9ef544e110174a205b7
mmtol <mmigueltolosana@gmail.com>
2026-01-14 09:47:14 +0100

    Form04

---
+++ NetCoreAdoNet/Form04EliminarPlantilla.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form04EliminarPlantilla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtId = new TextBox();
+            lstPlantilla = new ListBox();
+            label2 = new Label();
+            btnEliminar = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(14, 45);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(100, 23);
+            txtId.TabIndex = 10;
+            // 
+            // lstPlantilla
+            // 
+            lstPlantilla.FormattingEnabled = true;
+            lstPlantilla.Location = new Point(158, 45);
+            lstPlantilla.Name = "lstPlantilla";
+            lstPlantilla.Size = new Size(164, 199);
+            lstPlantilla.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(158, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Plantilla";
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(14, 89);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(75, 44);
+            btnEliminar.TabIndex = 7;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Inscripcion";
+            // 
+            // Form04EliminarPlantilla
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(352, 260);
+            Controls.Add(txtId);
+            Controls.Add(lstPlantilla);
+            Controls.Add(label2);
+            Controls.Add(btnEliminar);
+            Controls.Add(label1);
+            Name = "Form04EliminarPlantilla";
+            Text = "Form04EliminarPlantilla";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtId;
+        private ListBox lstPlantilla;
+        private Label label2;
+        private Button btnEliminar;
+        private Label label1;
+    }
+}
---
+++ NetCoreAdoNet/Form04EliminarPlantilla.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form04EliminarPlantilla : Form
+    {
+        SqlConnection conn;
+        SqlCommand command;
+        SqlDataReader reader;
+
+        public Form04EliminarPlantilla()
+        {
+            InitializeComponent();
+
+            string stringConn = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+
+            conn = new SqlConnection(stringConn);
+            command = new SqlCommand();
+
+            LoadPlantilla();
+        }
+
+        private void CrearCommand(string sql)
+        {
+            command.Connection = conn;
+            command.CommandType = CommandType.Text;
+            command.CommandText = sql;
+        }
+
+        private void LoadPlantilla()
+        {
+            lstPlantilla.Items.Clear();
+
+            CrearCommand("select EMPLEADO_NO, APELLIDO from PLANTILLA");
+            conn.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string id = reader["EMPLEADO_NO"].ToString();
+                string apellido = reader["APELLIDO"].ToString();
+
+                lstPlantilla.Items.Add(id + " - " + apellido);
+            }
+
+            reader.Close();
+            conn.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text;
+            CrearCommand("delete from PLANTILLA where EMPLEADO_NO=" + id);
+            conn.Open();
+            int registros = command.ExecuteNonQuery();
+            conn.Close();
+
+            LoadPlantilla();
+            MessageBox.Show("Trabajadores eliminados: " + registros);
+        }
+    }
+}
